e2b368747b1bb3f7dcf2c4a6d354eb7a5e849b41
root <manuelantognazza@gmail.com>
2024-05-11 16:21:36 -0300

    007

--- debugging/mines.py
+++ debugging/mines.py
@@ -61,6 +61,11 @@ class Minesweeper:
     def play(self):
         while True:
             self.print_board()
+            if self.check_win():
+                self.print_board(reveal=True)
+                print("Congratulations! You've won the game.")
+                break
+
             try:
                 x = int(input("Enter x coordinate: "))
                 y = int(input("Enter y coordinate: "))
@@ -69,11 +74,7 @@ class Minesweeper:
                     print("Game Over! You hit a mine.")
                     break
 
-                if self.check_win():
-                    self.print_board(reveal=True)
-                    print("Congratulations! You've won the game.")
-                    break
-
+                
             except ValueError:
                 print("Invalid input. Please enter numbers only.")
 
